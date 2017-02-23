b0da2ab4e6001b6a5e69141657b1f5f18cd10ab1
miaozhihao001 <1026145686@qq.com>
2017-02-23 23:28:56 +0800

    java8

--- java8/src/main/java/com/zhihao/miao/PredicateTest.java
+++ java8/src/main/java/com/zhihao/miao/PredicateTest.java
@@ -3,6 +3,7 @@ package com.zhihao.miao;
 import java.util.function.Predicate;
 
 public class PredicateTest {
+	
 	public static void main(String[] args) {
 		Predicate<String> pre = p -> p.length() > 5;
 		System.out.println(pre.test("hello"));
