892744c565e079eb465ef5ec639457fa1a484ae1
miaozhihao001 <1026145686@qq.com>
2017-03-27 23:36:02 +0800

    java8/

--- java8/src/main/java/com/zhihao/stream/source/StreamTest2.java
+++ java8/src/main/java/com/zhihao/stream/source/StreamTest2.java
@@ -5,7 +5,13 @@ import java.util.List;
 
 public class StreamTest2 {
     public static void main(String[] args) {
-        List<String> list = Arrays.asList("hello","world","hello world");
+        List<String> list = Arrays.asList("hello","world","hello world","welcome","person","student");
         list.stream().forEach(System.out::println);
+        list.stream().map(str -> str+"").forEach(System.out::println);
+
+        //使用并行流执行的顺序未知，看看forEach的api就知道原因了
+        list.parallelStream().forEach(System.out::println);
+
+
     }
 }
--- java8/src/main/java/com/zhihao/stream/source/StreamTest4.java
+++ java8/src/main/java/com/zhihao/stream/source/StreamTest4.java
@@ -1,7 +1,4 @@
 package com.zhihao.stream.source;
 
-/**
- * Created by Administrator on 2017/3/24.
- */
 public class StreamTest4 {
 }
