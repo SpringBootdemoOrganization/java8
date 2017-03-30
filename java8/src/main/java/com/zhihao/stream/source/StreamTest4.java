5b3d81f18ed60e44834c470a67ed76cf96f8e470
苗志浩 <naeshihiroshi@miaozhihaodeMacBook-Pro.local>
2017-03-31 00:16:03 +0800

    src/

--- java8/src/main/java/com/zhihao/stream/source/StreamTest4.java
+++ java8/src/main/java/com/zhihao/stream/source/StreamTest4.java
@@ -1,4 +1,17 @@
 package com.zhihao.stream.source;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class StreamTest4 {
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("hello","world","hello world","welcome","person","student");
+
+        System.out.println(list.getClass());//class java.util.Arrays$ArrayList
+
+
+        list.stream().forEach(System.out::println);
+
+        list.stream().map(item -> item).filter(item -> true).forEach(System.out::println);
+    }
 }
